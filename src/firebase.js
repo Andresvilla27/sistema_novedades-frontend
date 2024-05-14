e1a0c2662a7f164f90b353cfb25c4063bd53c7b9
Andres Villafañe <prevenciondeptal@gmail.com>
2024-05-13 23:19:43 -0300

    cambio de app de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -5,14 +5,23 @@ import { v4 } from 'uuid';
 import 'firebase/firestore';
 
 
-const firebaseConfig = {
+/* const firebaseConfig = {
   apiKey: "AIzaSyANtEspivOFRGNptiCAh4fAm7Fr63Hpdwc",
   authDomain: "novedades-prevencion.firebaseapp.com",
   projectId: "novedades-prevencion",
   storageBucket: "novedades-prevencion.appspot.com",
   messagingSenderId: "275564188826",
   appId: "1:275564188826:web:af68145b033e61ae9f3b23"
+}; */
+const firebaseConfig = {
+  apiKey: "AIzaSyAX4WuTio-ERViKqciwKl8I1jPcVkvL4Kc",
+  authDomain: "novedades-identificaciones.firebaseapp.com",
+  projectId: "novedades-identificaciones",
+  storageBucket: "novedades-identificaciones.appspot.com",
+  messagingSenderId: "69639742662",
+  appId: "1:69639742662:web:37171383991b0de74a63c7"
 };
+
 
 const app = initializeApp(firebaseConfig)
 
